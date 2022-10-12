5d995aed2e87fc039ab1f5f2b75067e52a18e540
martin <martin@comets.kr>
2022-10-12 21:45:48 +0900

    feat: restaurant screen develop chosenDish component

--- src/app/screens/RestaurantPage/chosenDish.tsx
+++ src/app/screens/RestaurantPage/chosenDish.tsx
@@ -1,5 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Stack, Box } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import Marginer from "../../components/marginer";
+import Button from "@mui/material/Button";
+import Rating from "@mui/material/Rating";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { FreeMode, Navigation, Thumbs } from "swiper";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import Checkbox from "@mui/material/Checkbox";
+
+const chosen_list = Array.from(Array(3).keys());
 
 export function ChosenDish() {
-  return <div className="chosen_dish_page"></div>;
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+  return (
+    <div className="chosen_dish_page">
+      <Container className="dish_container">
+        <Stack className="chosen_dish_slider">
+          <Swiper
+            className="dish_swiper"
+            loop={true}
+            spaceBetween={10}
+            navigation={true}
+            // thumbs={{ swiper: thumbsSwiper }}
+            modules={[FreeMode, Navigation, Thumbs]}
+          >
+            {chosen_list.map((ele) => {
+              const image_path = `/others/sandvich.jpg`;
+              return (
+                <SwiperSlide>
+                  <img
+                    style={{ width: "100%", height: "100%" }}
+                    src={image_path}
+                  />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+          <Swiper
+            // onSwiper={setThumbsSwiper}
+            loop={true}
+            spaceBetween={50}
+            slidesPerView={3}
+            freeMode={true}
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper"
+            style={{ width: "450px", height: "245px", marginTop: "20px" }}
+          >
+            {chosen_list.map((ele, index) => {
+              const image_path = `/others/sandvich.jpg`;
+              return (
+                <SwiperSlide
+                  style={{
+                    height: "107px",
+                    display: "flex",
+                  }}
+                >
+                  <img style={{ borderRadius: "15px" }} src={image_path} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </Stack>
+        <Stack className={"chosen_dish_info_container"}>
+          <Box className={"chosen_dish_info_box"}>
+            <strong className={"dish_txt"}>Sweet Sandvich</strong>
+            <span className={"resto_name"}>Texas De Brazil</span>
+            <Box className={"rating_box"}>
+              <Rating name="half-rating" defaultValue={3.5} precision={0.5} />
+              <div className={"evaluation_box"}>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginRight: "20px",
+                  }}
+                >
+                  <Checkbox
+                    {...label}
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite style={{ color: "red" }} />}
+                    /*@ts-ignore*/
+                    checked={true}
+                  />
+
+                  <span>98 ta</span>
+                </div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <RemoveRedEyeIcon sx={{ mr: "10px" }} />
+                  <span>1000 ta</span>
+                </div>
+              </div>
+            </Box>
+            <p className={"dish_desc_info"}>Juda mazzali sandvich</p>
+            <Marginer
+              direction="horizontal"
+              height="1"
+              width="100%"
+              bg="#000000"
+            />
+            <div className={"dish_price_box"}>
+              <span>Narx:</span>
+              <span>$11</span>
+            </div>
+            <div className={"button_box"}>
+              <Button variant="contained">Savatga qo'shish</Button>
+            </div>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
