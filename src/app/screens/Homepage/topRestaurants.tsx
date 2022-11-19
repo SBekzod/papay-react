bdef240e7d99183cb3d7d88e5103777737cc32f3
martin <martin@comets.kr>
2022-11-19 17:13:18 +0900

    fix: modify retrieve top restaurants component

--- src/app/screens/Homepage/topRestaurants.tsx
+++ src/app/screens/Homepage/topRestaurants.tsx
@@ -105,7 +105,7 @@ export function TopRestaurants() {
                         <Favorite
                           style={{
                             fill:
-                              ele?.me_liked && ele?.me_liked[0].my_favorite
+                              ele?.me_liked && ele?.me_liked[0]?.my_favorite
                                 ? "red"
                                 : "white",
                           }}
