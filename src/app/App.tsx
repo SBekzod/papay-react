6e2a2e627a6db71c34ae4a1e24a8cd390ceadd64
martin <martin@comets.kr>
2022-11-19 18:54:08 +0900

    feat: publish modal popups on papay authentication process

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -16,6 +16,7 @@ import { NavbarHome } from "./components/header";
 import { NavbarRestaurant } from "./components/header/restaurant";
 import { NavbarOthers } from "./components/header/others";
 import { Footer } from "./components/footer";
+import AuthenticationModal from "./components/auth";
 
 function App() {
   const [path, setPath] = useState();
@@ -56,6 +57,8 @@ function App() {
       </Switch>
 
       <Footer />
+
+      <AuthenticationModal />
     </Router>
   );
 }
