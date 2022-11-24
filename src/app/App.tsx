16199acd1a94950a51195027d71a8ed06437ee85
martin <martin@comets.kr>
2022-11-25 03:53:49 +0900

    fix: enable basket features within all navbars

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -151,6 +151,10 @@ function App() {
           handleCloseLogOut={handleCloseLogOut}
           handleLogOutRequest={handleLogOutRequest}
           verifiedMemberData={verifiedMemberData}
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
         />
       ) : main_path.includes("/restaurant") ? (
         <NavbarRestaurant
@@ -179,6 +183,10 @@ function App() {
           handleCloseLogOut={handleCloseLogOut}
           handleLogOutRequest={handleLogOutRequest}
           verifiedMemberData={verifiedMemberData}
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
         />
       )}
 
